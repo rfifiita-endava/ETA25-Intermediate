82582216731d33f8ab1d3502c89f845b34f2216d
ENDAVA\RFifiita <raduconstantin.fifiita@endava.com>
2025-03-31 13:12:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ETA25_Intermediate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
