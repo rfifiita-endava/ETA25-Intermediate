3d3517ad35e5f91e6ceec27f3124f6f25794cb16
ENDAVA\RFifiita <raduconstantin.fifiita@endava.com>
2025-04-23 18:28:08 +0300

    Session04

---
+++ Session04/Enums/Enums.cs
@@ -0,0 +1,25 @@
+﻿namespace ETA25_Intermediate.Session04.Enums;
+
+public enum CardName
+{
+    Elements,
+    Forms,
+    AlertsFramesWindows,
+    Widgets,
+    Interactions,
+    BookStoreApplication
+}
+
+public enum Gender
+{
+    Male,
+    Female,
+    Other
+}
+
+public enum Hobby
+{
+    Sports,
+    Reading,
+    Music
+}
---
+++ Session04/PracticeFormTests.cs
@@ -0,0 +1,57 @@
+﻿using ETA25_Intermediate.Session04.Enums;
+using ETA25_Intermediate.Session04.HelperMethods;
+using ETA25_Intermediate.Session04.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ETA25_Intermediate.Session04;
+public class PracticeFormTests
+{
+    public IWebDriver Driver;
+    public const string BaseUrl = "https://demoqa.com/";
+    public JavascriptHelper JavascriptHelper;
+    private readonly PracticeFormPage _practiceFormPage;
+    private readonly HomePage _homePage;
+
+    public PracticeFormTests()
+    {
+        Driver = new ChromeDriver();
+        JavascriptHelper = new JavascriptHelper(Driver);
+        _practiceFormPage = new PracticeFormPage(Driver);
+        _homePage = new HomePage(Driver);
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        Driver.Navigate().GoToUrl(BaseUrl);
+
+        Driver.Manage().Window.Maximize();
+    }
+
+    [TearDown]
+    public void CleanUp()
+    {
+        if (Driver != null)
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+    }
+
+    [Test]
+    public void PracticeFormTest1()
+    {
+        _homePage.AccessPageByName(CardName.Forms);
+
+        IWebElement practiceFormOption = Driver.FindElement(By.XPath("//span[text()=\"Practice Form\"]"));
+        practiceFormOption.Click();
+        JavascriptHelper.ScrollVertically(400);
+
+        _practiceFormPage.FillInFormFields("Radu", "Fifiita", "test@test.com", Gender.Male, "00010305", "1992", "April", "16",
+            new List<string>() { "English", "Accounting", "Physics" }, new List<Hobby>() { Hobby.Music, Hobby.Reading }, "Test Address 123, &*()");
+
+        Thread.Sleep(3000);
+    }
+}
